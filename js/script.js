9438f51e00ec773f65319096f3cdd47b9f65f771
Bruno de Paula dos Santos <bruno_ps94@hotmail.com>
2024-05-08 15:22:00 -0300

    Ajustes mobile

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const menu = document.querySelector(".headerEsquerda");
+    if (menu.style.display == 'flex'){
+        menu.style.display = 'none'
+    }else {
+        menu.style.display = 'flex'
+    }
+}
