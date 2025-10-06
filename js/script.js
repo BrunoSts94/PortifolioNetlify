ec459aa1d5cea48dc953d5da23118a50dbb56c2a
BrunoSts94 <bruno_ps94@hotmail.com>
2025-10-06 12:10:47 -0300

    Ajuste de links do github

--- js/script.js
+++ js/script.js
@@ -34,42 +34,42 @@ function showInfoProjs(valorP){
     switch(valorP) {
 		case p1: 
         textInfo.innerHTML = "Neste projeto pude aprender um pouco mais sobre o efeito de carrocel em um site, utilizando funcionalidades do Javascript. Foi utilizado imagens do site do jogo World of Tanks, o link do site encontra-se no botão saiba mais."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/Carrossel%20WoT/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/carrocel-world-of-tanks/index.html'
 		break
 
 		case p2: 
         textInfo.innerHTML = "Neste projeto trabalhei um pouco sobre modelos e a criação de landing pages, utilizando como exemplo uma empresa fictícia que criei em meu TCC na faculdade. O projeto também tem o objetivo de exercitar a responsividade na criação de sites."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/LandingPage%20Gerenfacil/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/gerenfacil/index.html'
 		break
 
         case p3: 
         textInfo.innerHTML = "Neste projeto foi exercitado funcionalidades do javascript, o projeto tem como objetivo fazer ligação com uma API, e retornar informações climaticas conforme a localidade informada."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/ProjetoClimaTempo/'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/clima-tempo/index.html'
 		break
 
         case p4: 
         textInfo.innerHTML = "Neste projeto foi exercitado funcionalidades do javascript, para criação de um formulario escondido, máscara de fundo, criação de botões para direcionamento para redes sociais, e recebimento de formularios."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/Proj%20Mario/'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/mario-brothers/index.html'
 		break
 
         case p5: 
         textInfo.innerHTML = "Neste projeto pude aprender mais funcionalidades do javascript, criando um ajustador de cores."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/Proj.%20Ajustador%20RGB/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/ajustador-rgb/index.html'
 		break
 
         case p6: 
         textInfo.innerHTML = "Neste projeto foi exercitado posicionamento de objetos e seleção na tela, com javascript. também foi um projeto voltado a trabalhar a responsividade."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/X-men/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/X-men/index.html'
 		break
 
         case p7: 
         textInfo.innerHTML = "Neste projeto pude aprender um pouco mais sobre o efeito paralax em um site, foi um projeto simples mas que gostei bastante, pois pude explorar essse efeito interessandte juntamente com o uso de mídia."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/proj-parallax/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/parallax/index.html'
 		break
 
         case p8: 
         textInfo.innerHTML = "Projeto realizado como exercício para criação de uma pagina empresarial, aplicando responsividade."
-        linkPage.href = 'https://brunosts94.github.io/MeuPortifolio/Landing%20Page%20Empresa/index.html'
+        linkPage.href = 'https://brunosts94.github.io/LandingPages_Portifolio/agencia-crescer/index.html'
 		break
 
         case p9: 
